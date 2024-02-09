c995493b7b4e11d4326a6eeb804f099d2b2e86e7
Shariful Islam <sijanewubd@gmail.com>
2024-02-10 01:24:52 +0600

    added unit test for getPostById method

--- src/test/java/com/springboot/blog/Service/PostServiceTest.java
+++ src/test/java/com/springboot/blog/Service/PostServiceTest.java
@@ -2,8 +2,8 @@ package com.springboot.blog.Service;
 
 import com.springboot.blog.entity.Post;
 import com.springboot.blog.repository.PostRepository;
-import com.springboot.blog.service.PostService;
 import com.springboot.blog.service.impl.PostServiceImpl;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -25,12 +26,22 @@ public class PostServiceTest {
     PostServiceImpl postServiceImpl;
 
     @Test
-    public void test_getAllPosts(){
-        List<Post> myPosts =new ArrayList<Post>();
-        myPosts.add(new Post(1L,"This is my 1st post","1st post description","This is my 1st post"));
-        myPosts.add(new Post(2L,"This is my 2nd post","2nd post description","This is my 2nd post"));
+    @Order(2)
+    public void test_getAllPosts() {
+        List<Post> myPosts = new ArrayList<Post>();
+        myPosts.add(new Post(1L, "This is my 1st post", "1st post description", "This is my 1st post"));
+        myPosts.add(new Post(2L, "This is my 2nd post", "2nd post description", "This is my 2nd post"));
         when(postRepository.findAll()).thenReturn(myPosts);
-        assertEquals(2,postServiceImpl.getAllPosts().size());
-
+        assertEquals(2, postServiceImpl.getAllPosts().size());
     }
+
+    @Test
+    @Order(3)
+    public void test_getPostById() {
+        Post post = new Post(1L, "This is my 1st post", "1st post description", "This is my 1st post");
+        Long id = 1L;
+        when(postRepository.findById(id)).thenReturn(Optional.of(post));
+        assertEquals(id, postServiceImpl.getPostById(id).getId());
+    }
+
 }
